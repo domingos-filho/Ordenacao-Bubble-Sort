3de7485269012b519699ebf440e09bf57ff2f16f
Francisco Antonio Domingos Filho <francisco_adf@hotmail.com>
2020-06-23 21:52:42 -0300

    Codigo de comparacao do array

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,25 +4,31 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		int[] array = { 9, 5, 1, 3, 4, 7, 2 };
+		int[] array = { 5, 3, 8, 2, 6 };
 		int aux = 0;
-		int cont = 0;
+
+		System.out.print("Array desordenado = { ");
+		for (int vect : array) {
+			System.out.print(vect + ", ");
+		}
+		System.out.println("}");
+
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array.length - 1; j++) {
 				if (array[j] > array[j + 1]) {
 					aux = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = aux;
-					cont += j;
-
+				
 				}
 			}
 		}
 
+		System.out.print("Array ordenado = { ");
 		for (int vect : array) {
 			System.out.print(vect + ", ");
 		}
-		System.out.println(cont);
+		System.out.println("}");
 	}
 
 }
